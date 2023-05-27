587db769415781a2c4629cbad4da8b89d7fec270
smzzl <318611006@qq.com>
2023-05-28 02:44:02 +0800

    Delete model_v0 directory

--- model_v0/Models/Cloud2textFeatureModel.py
+++
@@ -1,8 +0,0 @@
-import torch
-class BaseLine(torch.nn.Module) :
-    def __init__(self) :
-        super(BaseLine, self).__init__()
-        
-    def forward(self) :
-        return None
-    
--- model_v0/MyFunctional/classificationHead.py
+++
@@ -1,7 +0,0 @@
-import torch
-class CosineSim(torch.nn.Module) :
-    def __init__(self) :
-        super(CosineSim, self).__init__()
-        
-    def forward(self, x: torch.tensor, y: torch.tensor) :
-        return x*y/x.norm(p=2)/y.norm(p=2)
